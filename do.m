c08158450037a971b044333766f9af26456d6f0c
Michael Wood-Vasey <wmwv@pitt.edu>
2016-10-26 16:38:37 -0400

    Streamline generation of mean_auc subsamples.

--- do.m
+++ do.m
@@ -5,18 +5,25 @@ second_night_sleep_restriction = ([pall(:).condnum] == 2) & ([pall(:).night] == 
 first_night_sleep_extension = ([pall(:).condnum] == 1) & ([pall(:).night] == 1);
 second_night_sleep_extension = ([pall(:).condnum] == 1) & ([pall(:).night] == 2);
 
-fn_sr = pall(first_night_sleep_restriction)
-sn_sr = pall(second_night_sleep_restriction)
-fn_se = pall(first_night_sleep_extension)
-sn_se = pall(second_night_sleep_extension)
+fn_sr = pall(first_night_sleep_restriction);
+sn_sr = pall(second_night_sleep_restriction);
+fn_se = pall(first_night_sleep_extension);
+sn_se = pall(second_night_sleep_extension);
 
-subject = 10
+subject = 10;
 
-p = sn_sr(subject)
+p = sn_sr(subject);
 
-freq_channel = 8  % The first few channels have the main power
-lead = 1
-time = datetime(p.TIME_20s)
+% Loose maximum of the power in a given frequency bin
+% Intended for use in plotting later.
+% There are 39 freq channels
+auc_freq_ylimit = 1e4 * ones([39]);
+auc_freq_ylimit(1:4) = [1e7, 1e6, 1e5, 1e4];
+
+freq_channel = 2;  % The first few channels have the main power
+lead = 1;
+time = datetime(p.TIME_20s);
+
 figure(1)
 clf
 subplot(3,1,1)
@@ -35,7 +42,6 @@ ylabel(sprintf('Accumulated Power in Freq Channel %d', freq_channel))
 
 % Plot the full set of subjects
 
-ylimit_freq = 1e6
 figure(2)
 clf
 subplot(2,2,1,'replace')
@@ -47,7 +53,7 @@ end
 title('First Night Sleep Extension')
 ylabel(sprintf('Accumulated Power in Freq Channel %d', freq_channel))
 xlim(gca, [0,400])
-ylim(gca, [0,ylimit_freq])
+ylim(gca, [0,auc_freq_ylimit(freq_channel)])
 
 subplot(2,2,2,'replace')
 hold on
@@ -57,7 +63,7 @@ for s = 1:length(sn_se)
 end
 title('Second Night Sleep Extension')
 xlim(gca, [0,400])
-ylim(gca, [0,ylimit_freq])
+ylim(gca, [0,auc_freq_ylimit(freq_channel)])
 
 subplot(2,2,3,'replace')
 hold on
@@ -69,7 +75,7 @@ title('First Night Sleep Restriction')
 ylabel(sprintf('Accumulated Power in Freq Channel %d', freq_channel))
 xlabel('Time-ordered samples')
 xlim(gca, [0,400])
-ylim(gca, [0,ylimit_freq])
+ylim(gca, [0,auc_freq_ylimit(freq_channel)])
 
 subplot(2,2,4,'replace')
 hold on
@@ -80,40 +86,18 @@ end
 title('Second Night Sleep Restriction')
 xlabel('Time-ordered samples')
 xlim(gca, [0,400])
-ylim(gca, [0,ylimit_freq])
+ylim(gca, [0,auc_freq_ylimit(freq_channel)])
 
-temp_auc=zeros([189,1759]);
+all_auc=zeros([len(pall),1759]);
 for i=1:length(pall)
     thisdata=squeeze(pall(i).auc_cum(lead,freq_channel,:));
-    temp_auc(i,1:length(thisdata))=thisdata;
+    all_auc(i,1:length(thisdata))=thisdata;
 end
 
-%%%
-fn_se_auc = zeros([length(fn_se), 1759]);
-for i=1:length(fn_se)
-    thisdata=squeeze(fn_se(i).auc_cum(lead,freq_channel,:));
-    fn_se_auc(i,1:length(thisdata))=thisdata;
-end
-
-sn_se_auc = zeros([length(sn_se), 1759]);
-for i=1:length(sn_se)
-    thisdata=squeeze(sn_se(i).auc_cum(lead,freq_channel,:));
-    sn_se_auc(i,1:length(thisdata))=thisdata;
-end
-
-
-fn_sr_auc = zeros([length(fn_sr), 1759]);
-for i=1:length(fn_sr)
-    thisdata=squeeze(fn_sr(i).auc_cum(lead,freq_channel,:));
-    fn_sr_auc(i,1:length(thisdata))=thisdata;
-end
-
-
-sn_sr_auc = zeros([length(sn_sr), 1759]);
-for i=1:length(sn_sr)
-    thisdata=squeeze(sn_sr(i).auc_cum(lead,freq_channel,:));
-    sn_sr_auc(i,1:length(thisdata))=thisdata;
-end
+fn_se_auc = all_auc(first_night_sleep_restriction,:);
+sn_se_auc = all_auc(second_night_sleep_restriction,:);
+fn_se_auc = all_auc(first_night_sleep_extension,:);
+sn_se_auc = all_auc(second_night_sleep_extension,:);
 
 mean_fn_se_auc = mean(fn_se_auc, 1);
 mean_sn_se_auc = mean(sn_se_auc, 1);
@@ -121,9 +105,21 @@ mean_fn_sr_auc = mean(fn_sr_auc, 1);
 mean_sn_sr_auc = mean(sn_sr_auc, 1);
 
 figure(3)
+clf
 hold on
-plot(mean_fn_se_auc)
-plot(mean_sn_se_auc)
-plot(mean_fn_sr_auc)
-plot(mean_sn_sr_auc)
-legend('Mean First Night Sleep Extension', 'Mean Second Night Sleep Extension', 'Mean First Night Sleep Restriction', 'Mean Second Night Sleep Restriction')
+plot(mean_fn_se_auc, 'DisplayName', 'Sleep Extension : First Night')
+plot(mean_sn_se_auc, 'DisplayName', 'Sleep Extension : Second Night')
+plot(mean_fn_sr_auc, 'DisplayName', 'Sleep Restriction : First Night')
+plot(mean_sn_sr_auc, 'DisplayName', 'Sleep Restriction : Second Night')
+plot(median(fn_se_auc,1), 'DisplayName', 'Sleep Extension : First Night')
+plot(median(sn_se_auc,1), 'DisplayName', 'Sleep Extension : Second Night')
+plot(median(fn_sr_auc,1), 'DisplayName', 'Sleep Restriction : First Night')
+plot(median(sn_sr_auc,1), 'DisplayName', 'Sleep Restriction : Second Night')
+
+xlabel('Time-ordered Samples')
+ylabel(sprintf('Accumulated Power in Freq Channel %d', freq_channel))
+title('Mean AUC across the Population')
+ylim([0, auc_freq_ylimit(freq_channel)])
+legend('show')
+
+
